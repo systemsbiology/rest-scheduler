3df06ca55746d5bac1662f0650e9ad1087afb806
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-03-17 15:58:08 -0700

    Monkey with spec helper to get things working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,21 +5,12 @@ require 'spec/autorun'
 require 'spec/interop/test'
 require 'rack/test'
 
-Test::Unit::TestCase.send :include, Rack::Test::Methods
-
-set :environment, :test
-
 def app
   RestScheduler::Server
 end
 
+set :environment, :test
+
 Spec::Runner.configure do |config|
-  # == Mock Framework
-  #
-  # RSpec uses it's own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.include Rack::Test::Methods
 end
